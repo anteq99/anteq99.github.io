9d8eb2c2ca267357c7c95b23465c00be003570df
anteq99 <antekking99@gmail.com>
2025-01-23 21:50:56 +0100

    Update script.js

--- games/blackjack/script.js
+++ games/blackjack/script.js
@@ -2,25 +2,23 @@
 let header = document.querySelector("header");
 let play = document.querySelector("#play");
 let main = document.querySelector("main");
-let betMoney = document.querySelector("#betmoney")
+let betBar = document.querySelector("#betbar")
+let betAccept = document.querySelector("#betaccept")
 let hit = document.querySelector("#hit");
 let stand = document.querySelector("#stand");
 if(localStorage.getItem("money") == null){
     localStorage.setItem("money", "1000")
 }
-let money = localStorage.getItem("money");
+let betMoney = 0;
 let deck = [];
-let dealer = [main.querySelector("#dealer"),0,0] // html,value of cards, number of aces
-let player = [main.querySelector("#player"),0,0]
+let dealer, player, money; 
 //---------------------------------------
 play.addEventListener("click", function(){
     play.style.setProperty("display","none");
-    header.innerHTML = ""
-    //bet();
-    draw(player);
-    draw(player);
-    draw(dealer);
-    game()
+    money = localStorage.getItem("money");
+    dealer = [main.querySelector("#dealer"),0,0] // html,value of cards, number of aces
+    player = [main.querySelector("#player"),0,0]
+    bet();
 })
 hit.addEventListener("click",function(){
     draw(player)
@@ -34,10 +32,33 @@ stand.addEventListener("click",function(){
     stand.style.setProperty("display","none");
     standed()
 })
+betAccept.addEventListener("click",function(){
+    let temp = parseInt(betBar.value)
+    if(temp <= money){
+        betMoney = temp;
+        header.style.setProperty("display","none")
+        betAccept.style.setProperty("display","none")
+        betBar.style.setProperty("display","none")
+        game()
+    }
+})
 //---------------------------------------
 function game(){
+    draw(player);
+    draw(player);
+    draw(dealer);
     let back = document.createElement("sect");
     back.classList.add("card");
+    back.style.setProperty("right","10vw")
+    let right = -20
+    let anim = setInterval(function(){
+        right+=0.5
+        back.style.setProperty("top",right+"vw")
+        if(right==0){
+            clearInterval(anim);
+        }
+    },20)
+    
     dealer[0].appendChild(back)
     setTimeout(function(){
         hit.style.setProperty("display","block");
@@ -46,56 +67,76 @@ function game(){
 }
 function standed(){
     draw(dealer)
-    if(dealer[1] < 17){
-        setTimeout(function(){
-            standed()
-        },1000)
-        
-    }
-    else{
-        if(ifBusted(dealer)){
-        end("w");
-        }
-        else{
-            compare()
-        }
-    }
-}
-function end(wl){
-    if(wl=="w"){
-        setTimeout(function(){
-            alert("WYGRAŁÆŚ")
-        },1000)
-    }
-    if(wl=="l"){
-        setTimeout(function(){
-            alert("PRZEGRAŁÆŚ")
-        },1000)
-    }
-}
-function compare(){
     let dpts = dealer[1];
     let ppts = player[1];
-    while(dealer[1]>21 && dealer[2] > 0){
+    while(dpts>21 && dealer[2] > 0){
         dpts -= 10;
         dealer[2] -=1;
     }
-    while(player[1]>21 && player[2] > 0){
+    while(ppts>21 && player[2] > 0){
         ppts -= 10;
         player[2] -=1;
     }
     console.log(dpts,ppts);
-    if(dpts > ppts){
-        end("l");
+    setTimeout(function(){
+        if(dpts >= 17){
+            if(ifBusted(dealer)){
+                end("w")
+            }else{
+                if(dpts > ppts){
+                    end("l");
+                }
+                if(dpts < ppts){
+                    end("w");
+                }
+                if(dpts == ppts){
+                    end("d");
+                }
+            }
+        }
+        else{
+            standed()
+        }
+    },1000)
+    
+}
+function end(wl){
+    if(wl=="w"){
+        setTimeout(function(){
+            alert("WYGRANA")
+            localStorage.setItem("money",parseInt(money)+parseInt(betMoney))
+        },1000)
     }
-    if(dpts < ppts){
-        end("w");
+    if(wl=="l"){
+        setTimeout(function(){
+            alert("PRZEGRANA")
+            localStorage.setItem("money",money-betMoney)
+        },1000)
     }
+    if(wl=="d"){
+        setTimeout(function(){
+            alert("REMIS")
+        },1000)
+    }
+    setTimeout(function(){
+        while(dealer[0].querySelectorAll(".card").length>0){
+            dealer[0].removeChild(dealer[0].querySelector(".card"));
+        }
+        while(player[0].querySelectorAll(".card").length>0){
+            player[0].removeChild(player[0].querySelector(".card"));
+        }
+        hit.style.setProperty("display","none");
+        stand.style.setProperty("display","none");
+        play.style.setProperty("display","block")
+        header.style.setProperty("display","block")
+        header.innerHTML = "Zagraj ponownie"
+    },2000)
+    
 }
 function bet(){
-    header.innerHTML = "Obstaw pieniądze"
-    header.style.setProperty("display","block")
-    betMoney.style.setProperty("display","block")
+    header.innerHTML = "Obstaw pieniądze<br><span style='font-size:3vw;'>Twoje saldo: "+ money + "</span>"
+    betAccept.style.setProperty("display","block");
+    betBar.style.setProperty("display","block");
 }
 function draw(dest){
     let numb, type, cardId;
@@ -118,6 +159,7 @@ function draw(dest){
     let card = document.createElement("div");
     dest[0].appendChild(card)
     card.classList.add("card");
+    card.style.setProperty("right",dest[0].querySelectorAll(".card").length*5+"vw")
     card.innerHTML = numb
     spin(card,numb,type);
     updValue(dest,card)
