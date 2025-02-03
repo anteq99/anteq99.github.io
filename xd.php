99cafb8b6132f4a310b70b813c5466f195aa8491
anteq99 <antekking99@gmail.com>
2025-02-03 20:05:16 +0100

    Update xd.php

--- xd.php
+++ xd.php
@@ -1,1 +1,16 @@
 
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A99 | Strona główna</title>
+    <link rel="icon" type="image/x-icon" href="photos/favicon.ico">
+</head>
+<body>
+  <?php
+echo "xd";
+?>
+</body>
+</html>
+
